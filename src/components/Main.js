bc9130ae11efc071362ce0526651e576e22de14a
bene <matheusbenediht@gmail.com>
2023-05-13 23:28:34 -0300

    Adicionando icones de editar e excluir tarefas

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 //Form
 import { FaPlus } from 'react-icons/fa';
+//Tarefas
+import { FaEdit, FaWindowClose} from 'react-icons/fa';
 import './Main.css';
 export default class Main extends Component {
   state = {
     novaTask: '',
+    tarefas: ['Pagar as contas', 'Bebe água', 'Estudar React'],
   };
 
   handleChange = (e) => {
@@ -13,17 +16,28 @@ export default class Main extends Component {
     });
   };
   render() {
-    const { novaTarefa } = this.state;
+    const { novaTarefa, tarefas } = this.state;
     return (
       <div className="main">
-        <h1>Lista ed Tarefas</h1>
+        <h1>Lista de Tarefas</h1>
 
         <form className="form" action="#">
-          <input onChange={this.handleChange} type="text" />
+          <input onChange={this.handleChange} type="text" value={novaTarefa} />
           <button type="submit">
             <FaPlus />
           </button>
         </form>
+        <ul className="tarefas">
+          {tarefas.map((tarefa) => (
+            <li key={tarefa}>
+                {tarefa}
+                <div>
+                    <FaEdit className='edit' />
+                    <FaWindowClose className='delete' />
+                </div>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
